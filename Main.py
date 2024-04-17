ea2ef262f1a3e47b11c650471d85952bb5c2f176
edo <edoardodrd@gmail.com>
2024-04-17 12:54:40 -0300

    working code 1st version

---
+++ Main.py
@@ -0,0 +1,25 @@
+import requests
+#constants
+
+world_filter="illum"
+
+#HTTP request
+
+resp = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+if resp.status_code == 200:
+
+    data = resp.json()
+    title_list = [todo ["title"] for todo in data]
+    filtered_titles = [title for title in title_list if world_filter in title.split()]
+    print("Filtered Titles:")
+    for title in filtered_titles:
+        print(f"- {title}") #use f-string to format and print all the filtered titles
+
+    #type checking the list
+    if all(isinstance(title, int) for title in filtered_titles):
+        print("There is at least one INT data type in the list")
+    else:
+        print("The list contains no INT data type")
+else:
+    print("HTTP Get request failed, status code:", resp.status_code)
